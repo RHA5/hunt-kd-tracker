543d7a2691de2da5a74ebdae634048763a1040b4
RHA5 <tapo4ek007@gmail.com>
2026-07-27 22:37:03 +0300

    base

---
+++ supabase.js
@@ -0,0 +1,5 @@
+const supabaseUrl = 'https://tcdtgptiwjmoxugfjvmh.supabase.co';
+const supabaseKey = 'sb_publishable_8XdnitORqFlCJLTnGnrlkw_rK9rAHF1';
+const supabase = supabasejs.createClient(supabaseUrl, supabaseKey);
+
+window.supabase = supabase;
