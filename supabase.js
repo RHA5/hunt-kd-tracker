80d2b10e09e727dd46baa970c65a8320b36b5ebf
RHA5 <tapo4ek007@gmail.com>
2026-07-27 22:42:59 +0300

    sb

--- supabase.js
+++ supabase.js
@@ -1,5 +1,7 @@
 const supabaseUrl = 'https://tcdtgptiwjmoxugfjvmh.supabase.co';
 const supabaseKey = 'sb_publishable_8XdnitORqFlCJLTnGnrlkw_rK9rAHF1';
-const supabase = supabasejs.createClient(supabaseUrl, supabaseKey);
+
+// Используем правильное имя глобального объекта из CDN v2
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 window.supabase = supabase;
